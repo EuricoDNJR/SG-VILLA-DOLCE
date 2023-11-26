c1f864e03a9581f9dd98e56b4e6cde3943473e07
EuricoDNJR <massaultyplay@gmail.com>
2023-11-25 23:55:21 -0300

    Add update_cliente function to CRUD module

--- app/database/crud.py
+++ app/database/crud.py
@@ -87,3 +87,40 @@ def get_all_clientes():
     except DoesNotExist:
         # Se ocorrer uma exceção DoesNotExist, retorna None
         return None
+
+def update_cliente(uuid, telefone=None, email=None, nome=None, dataNascimento=None, cpf=None, endereco=None, saldo=None):
+    try:
+        cliente = models.Cliente.get(models.Cliente.idCliente == uuid)
+        if cliente is None:
+            return None
+        # Atualiza os atributos fornecidos
+        if telefone is not None:
+            cliente.telefone = telefone
+        if email is not None:
+            cliente.email = email
+        if nome is not None:
+            cliente.nome = nome
+        if dataNascimento is not None:
+            cliente.dataNascimento = dataNascimento
+        if cpf is not None:
+            cliente.cpf = cpf
+        if endereco is not None:
+            cliente.endereco = endereco
+        if saldo is not None:
+            cliente.saldo = saldo
+
+        cliente.save()
+
+        return {
+            "idCliente": str(cliente.idCliente),
+            "email": cliente.email if cliente.email is not None else None,
+            "nome": cliente.nome,
+            "dataNascimento": cliente.dataNascimento.isoformat() if cliente.dataNascimento is not None else None,
+            "cpf": cliente.cpf if cliente.cpf is not None else None,
+            "endereco": cliente.endereco if cliente.endereco is not None else None,
+            "telefone": cliente.telefone if cliente.telefone is not None else None,
+            "saldo": str(cliente.saldo) if cliente.saldo is not None else None
+        }
+
+    except DoesNotExist:
+        return None
