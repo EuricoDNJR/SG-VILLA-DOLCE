48f4e737298383264ed616c86cf235d230ec1f8c
Lucas Lopes <lucaslopes072013@gmail.com>
2023-11-27 12:10:14 -0300

    Imprime mensagem de falha no login

--- frontend/login/assets/scripts/main.js
+++ frontend/login/assets/scripts/main.js
@@ -27,17 +27,16 @@ function requestLogin(){
     fetch("http://127.0.0.1:8000/v1/usuario/login/", options).then(
         (response) => {
             if(!response.ok){
-                // APRESENTAR NA TELA A FALHA NO LOGIN
-                if(response.status == 404){
-                    // USUÁRIO NÃO ENCONTRADO
-                    throw new Error(`${response.status} ${response.statusText}`);
-                }else if(response.status == 401){
-                    // USUÁRIO NÃO AUTORIZADO
-                    throw new Error(`${response.status} ${response.statusText}`);
-                }else{
-                    // FALHA NO LOGIN
-                    throw new Error(`${response.status} ${response.statusText}`);
-                }
+                const responseJsonPromise = response.json();
+
+                responseJsonPromise.then(
+                    (message) => {
+                        const errorMessage = document.getElementById("error-message");
+
+                        errorMessage.textContent = message;
+                        errorMessage.style.display = block;
+                    }
+                )
             }else{
                 // USUÁRIO AUTORIZADO
                 // REDIRECIONAR PARA PÁGINA DASHBOARD
