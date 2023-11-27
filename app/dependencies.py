ebfd70d52b01ebb633f10c4608c287777ee677a8
EuricoDNJR <massaultyplay@gmail.com>
2023-11-27 10:44:13 -0300

    Create auth with uuid(jwt)

---
+++ app/dependencies.py
@@ -0,0 +1,34 @@
+import os
+
+import dotenv
+from fastapi import Header, HTTPException, status
+from .database.crud import get_usuario_by_id
+
+dotenv.load_dotenv()
+
+TEST = os.getenv("TEST")
+
+async def get_token_header(jwt_token: str = Header()):
+    try:
+        if TEST == "ON":
+            if jwt_token != "test":
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid JWT token"
+                )
+
+            return "test"
+        
+        user = get_usuario_by_id(jwt_token)
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid JWT token"
+            )
+        
+        return jwt_token
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid JWT token"
+        )
+            
+        
+        
