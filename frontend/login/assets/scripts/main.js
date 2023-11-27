388564f2023730461bba2b2e79c6274d523be85e
Lucas Lopes <lucaslopes072013@gmail.com>
2023-11-26 21:30:40 -0300

    Tratamento de error no login

--- frontend/login/assets/scripts/main.js
+++ frontend/login/assets/scripts/main.js
@@ -1,13 +1,20 @@
 const btn = document.getElementById("btn");
 btn.addEventListener("click", requestLogin);
 
-function requestLogin(){
+function trataDadosLogin(){
     const cellphone = document.getElementById("cellphone");
     const password = document.getElementById("password");
-    const data = {
+    
+    // TRATAR DADOS AQUI 
+
+    return {
         "telefone": cellphone.value,
         "senha": password.value,
     };
+}
+
+function requestLogin(){
+    const data = trataDadosLogin();
 
     const options = {
         method: "POST",
@@ -17,13 +24,24 @@ function requestLogin(){
         body: JSON.stringify(data),  
     };
 
-    // fetch("http://127.0.0.1:8000/v1/usuario/login/", options).then(
-    //     (response) => {
-    //         console.log(response);
-    //         titulo.textContent = `${response.status} `;
-    //         return response.json();
-    //     }).then(
-    //         (jeison) => {
-    //         titulo.textContent += jeison.message;
-    //     })
+    fetch("http://127.0.0.1:8000/v1/usuario/login/", options).then(
+        (response) => {
+            if(!response.ok){
+                // APRESENTAR NA TELA A FALHA NO LOGIN
+                if(response.status == 404){
+                    // USUÁRIO NÃO ENCONTRADO
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }else if(response.status == 401){
+                    // USUÁRIO NÃO AUTORIZADO
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }else{
+                    // FALHA NO LOGIN
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }
+            }else{
+                // USUÁRIO AUTORIZADO
+                // REDIRECIONAR PARA PÁGINA DASHBOARD
+                // PRECISO DE ALGUNS DADOS DO USUÁRIO -> NOME, CARGO e IMAGEM
+            }
+        })
 }
