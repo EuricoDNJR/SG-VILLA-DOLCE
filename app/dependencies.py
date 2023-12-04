a5b2297cd26cde96363bdf6952782b72e513f552
EuricoDNJR <massaultyplay@gmail.com>
2023-12-04 16:44:35 -0300

    just add warning about test status

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,5 @@
 import os
-
+import logging
 import dotenv
 from fastapi import Header, HTTPException, status
 from .database.crud import get_usuario_by_id
@@ -11,6 +11,7 @@ TEST = os.getenv("TEST")
 async def get_token_header(jwt_token: str = Header()):
     try:
         if TEST == "ON":
+            logging.warning("Test mode")
             if jwt_token != "test":
                 raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid JWT token"
