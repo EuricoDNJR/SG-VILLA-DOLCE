668427cf36704e290d95909b21ae8f31d8d3e56e
EuricoDNJR <massaultyplay@gmail.com>
2023-12-04 23:28:56 -0300

    Add routes to get all products and get a specific product

--- app/routers/v1/produto.py
+++ app/routers/v1/produto.py
@@ -68,3 +68,53 @@ def create_product(data:CreateProductRequest, jwt_token: str = Header()):
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"message": "Erro ao criar produto: " + str(e)}
         )
+
+@router.get("/get_all_products/", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
+def get_all_products():
+    """
+    Retorna todos os produtos.
+    """
+    try:
+        logging.info("Getting user")
+        logging.info("Getting all products")
+        produtos = crud.get_all_produtos()
+        if produtos is None:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"message": "Erro ao buscar produtos"}
+            )
+        logging.info("Products found")
+        return JSONResponse(status_code=status.HTTP_200_OK, content=produtos)
+    except Exception as e:
+        logging.error(e)
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": "Erro ao buscar produtos: " + str(e)}
+        )
+
+@router.get("/get_product/{uuid}", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
+def get_product(uuid: str):
+    """
+    Retorna um produto.
+    """
+    try:
+        logging.info("Getting product")
+        produto = crud.get_product_by_id(uuid=uuid)
+        if produto is None:
+            logging.error("Product not found")
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        logging.info("Product found")
+        return JSONResponse(status_code=status.HTTP_200_OK, content={
+            "uuid": str(produto.idProduto), 
+            "Nome": produto.nome, 
+            "Descrição": produto.descricao if produto.descricao is not None else None, 
+            "Categoria": produto.categoria, 
+            "Valor de Custo": str(produto.valorCusto), 
+            "Valor de Venda": str(produto.valorVenda), 
+            "Unidade de Medida": produto.unidadeMedida})
+    except Exception as e:
+        logging.error(e)
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": "Erro ao buscar produto: " + str(e)}
+        )
