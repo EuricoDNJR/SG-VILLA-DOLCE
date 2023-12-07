0df170361ed2f4ba3c7aac7ef0bae3f4f9d46a6a
Lucas Lopes <lucaslopes072013@gmail.com>
2023-12-07 12:50:23 -0300

    Mode test on

--- frontend/electron-app/login/assets/scripts/main.js
+++ frontend/electron-app/login/assets/scripts/main.js
@@ -74,7 +74,7 @@ async function requestLogin(data){
     }
 }
 
-async function handleLogin(event){
+async function handleLogin(){
     resetErrorMessage();
 
     data = getDadosLoginForm();
@@ -84,17 +84,17 @@ async function handleLogin(event){
     if(formatedData.telefone === "" || formatedData.senha === ""){
         printErrorMessage("Preencha os campos de telefone e senha");
     }else{
-        userData = await requestLogin(formatedData);
+        // userData = await requestLogin(formatedData);
 
-        // const userData = {
-        //     token: "gd4fsd-0e9-af",
-        //     nome: "Eurico Delmondes do Nascimento Junior",
-        //     cargo: "Dev Back-END"
-        // };
+        const userData = {
+            token: "gd4fsd-0e9-af",
+            nome: "Eurico Delmondes do Nascimento Junior",
+            cargo: "Dev Back-END"
+        };
 
         if(userData){
             window.ipcRenderer.setUserDataCookie(userData).then(() => {
-                    window.ipcRenderer.redirectTo("dashboard/dashboard.html");
+                window.ipcRenderer.redirectTo("dashboard/dashboard.html");
             });
         } 
     }
