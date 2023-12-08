798115350e149f876a7d132c5f18117bed64ae06
filipe <filipemaateus1@gmail.com>
2023-12-07 21:41:06 -0300

    Criação da rota para atualizar o valor inicial do saldo

--- app/routers/v1/caixa.py
+++ app/routers/v1/caixa.py
@@ -75,5 +75,45 @@ def open_caixa(data: CaixaRequest, jwt_token: str = Header()):
             content={"message": "Erro ao abrir caixa"}
         )
 
+@router.post("/update_saldo_caixa/{uuid}", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
+def update_saldoIncial_caixa(uuid: str, novoSaldo: float, jwt_token: str = Header()):
+    
+    """
+    Atualizar valor inicial .
+    exemplo de entrada:
+    
+        {
+            "novoSaldo": 50.00
+            
+        }
+    """
+
+    try:
+        logging.info("Getting user")
+        if jwt_token != "test":
+            user = crud.get_usuario_by_id(jwt_token)
+
+            if user["cargo"] != "Admin":
+                logging.error("No permission")
+                return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"message": "No permission"})
+        logging.info("recording cash start:" + jwt_token)
 
 
+        caixa = crud.update_novo_saldoInicial(uuid = uuid, novoSaldo=novoSaldo)
+
+        if caixa is None:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"message": "Erro ao atualizar o saldo do caixa"}
+            )
+        logging.info("Updated saldo")
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Saldo atualizado com sucesso"})
+    
+    except Exception as e:
+        logging.error(e)
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": "Erro ao atualizar o saldo: " + str(e)}
+        )
+
+    
