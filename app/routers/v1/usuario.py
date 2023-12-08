8d7d12ce57867451e08b0d2b4ab031218ec7a6f1
EuricoDNJR <massaultyplay@gmail.com>
2023-12-08 03:11:10 -0300

    Just reformating request parameters to update user information

--- app/routers/v1/usuario.py
+++ app/routers/v1/usuario.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Optional
 from pydantic import BaseModel
 from passlib.hash import bcrypt
 from ...database import crud
@@ -173,18 +174,36 @@ def get_all_users(jwt_token: str = Header()):
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"message": "Erro ao buscar usuários: " + str(e)})
-    
+
+class UpdateUserRequest(BaseModel):
+    email: Optional[str] = None
+    senha: Optional[str] = None
+    nome: Optional[str] = None
+    dataNascimento: Optional[str] = None
+    cpf: Optional[str] = None
+    endereco: Optional[str] = None
+    telefone: Optional[str] = None
+    cargo: Optional[str] = None
+   
 @router.patch("/update_user/{idUsuario}", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
 def update_user(idUser: str, 
-                email: str =  None,
-                senha: str = None,
-                nome: str = None,
-                dataNascimento: str = None,
-                cpf: str = None,
-                endereco: str = None,
-                telefone: str = None,
-                cargo: str = None,
+                data: UpdateUserRequest,
                 jwt_token: str = Header()):
+    '''
+    Atualização de usuário.
+    exemplo de entrada:
+    
+        {
+            "email": "abashala@gmail.com",
+            "senha": "bombinhadorato",
+            "nome": "Cesar",
+            "dataNascimento": "2008/10/08",
+            "cpf": "987654321",
+            "endereco": "Rua fim de mundo",
+            "telefone": "40028922",
+            "cargo": "Funcionario"
+        }
+    '''
     try:
         logging.info("Verifying permission")
         if jwt_token != "test":
@@ -196,14 +215,14 @@ def update_user(idUser: str,
         logging.info("Updating user")
         update_user = crud.update_user(
             uuid=idUser,
-            email=email,
-            senha=senha,
-            nome=nome,
-            dataNascimento=dataNascimento,
-            cpf=cpf,
-            endereco=endereco,
-            telefone=telefone,
-            cargo=cargo
+            email=data.email,
+            senha=data.senha,
+            nome=data.nome,
+            dataNascimento=data.dataNascimento,
+            cpf=data.cpf,
+            endereco=data.endereco,
+            telefone=data.telefone,
+            cargo=data.cargo
         )
         if update_user is None:
             logging.error("User not found")
