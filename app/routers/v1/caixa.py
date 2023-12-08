0b7c24e40d58b21e8655591c1bd7b1e85acb0d37
filipe <filipemaateus1@gmail.com>
2023-12-08 16:57:44 -0300

    criaçãpo da rota close_caixa

--- app/routers/v1/caixa.py
+++ app/routers/v1/caixa.py
@@ -20,7 +20,7 @@ router = APIRouter()
 class CaixaRequest(BaseModel):
 
     saldoInicial: float
-    observacao: Optional[str]
+    observacoes: Optional[str]
 
 @router.post("/open_caixa/", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
 def open_caixa(data: CaixaRequest, jwt_token: str = Header()):
@@ -31,10 +31,10 @@ def open_caixa(data: CaixaRequest, jwt_token: str = Header()):
     
         {
             "saldoInicial": 100.00,
-            "observacao": "Saldo inicial"
+            "observacoes": "Saldo inicial"
         }
     """
-
+  
     try:
 
         logging.info("Getting user")
@@ -56,7 +56,7 @@ def open_caixa(data: CaixaRequest, jwt_token: str = Header()):
         caixa = crud.open_caixa(
             saldoInicial = data.saldoInicial,
             dataAbertura = dataAbertura,
-            observacao = data.observacao  
+            observacoes = data.observacoes
         )
 
         if caixa is None:
@@ -66,8 +66,10 @@ def open_caixa(data: CaixaRequest, jwt_token: str = Header()):
             )
         logging.info("Open box")
     
-        return JSONResponse(status_code=status.HTTP_200_OK, content={"SaldoInicial": caixa.saldoInicial, "DataAbertura": caixa.dataAbertura, "Observacoes": caixa.observacoes, "NomeFuncionario": user['nome']})
-
+        #retorno com nome do usuario
+        #return JSONResponse(status_code=status.HTTP_200_OK, content={"SaldoInicial": caixa.saldoInicial, "DataAbertura": caixa.dataAbertura, "Observacoes": caixa.observacoes, "NomeFuncionario": user['nome']})
+        
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"SaldoInicial": caixa.saldoInicial, "DataAbertura": caixa.dataAbertura, "Observacoes": caixa.observacoes})
     except Exception as e:
         logging.error(e)
         return JSONResponse(
@@ -75,8 +77,49 @@ def open_caixa(data: CaixaRequest, jwt_token: str = Header()):
             content={"message": "Erro ao abrir caixa"}
         )
 
+
+
+@router.post("/close_caixa/{uuid}", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
+def close_caixa(uuid: str, jwt_token: str = Header()):
+    
+    try:
+        logging.info("Getting user")
+        if jwt_token != "test":
+            user = crud.get_usuario_by_id(jwt_token)
+
+            if user["cargo"] != "Admin":
+                logging.error("No permission")
+                return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"message": "No permission"})
+        logging.info("recording cash start:" + jwt_token)
+
+        agora = datetime.now()
+        dataFechamento = agora.strftime("%Y-%m-%d %H:%M:%S")
+
+
+        caixa = crud.close_caixa(uuid = uuid, dataFechamento=dataFechamento)
+
+        if caixa is False:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"message": "Erro ao fechar o caixa"}
+            )
+        logging.info("Updated saldo")
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Caixa fechado com sucesso"})
+    
+    except Exception as e:
+        logging.error(e)
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": "Erro ao fechar o caixa: " + str(e)}
+        )
+    
+
+class novoSaldo(BaseModel):
+
+    novoSaldo: float
+
 @router.post("/update_saldo_caixa/{uuid}", status_code=status.HTTP_200_OK, dependencies=[Depends(get_token_header)])
-def update_saldoIncial_caixa(uuid: str, novoSaldo: float, jwt_token: str = Header()):
+def update_saldoIncial_caixa(data: novoSaldo, uuid: str, jwt_token: str = Header()):
     
     """
     Atualizar valor inicial .
@@ -99,7 +142,7 @@ def update_saldoIncial_caixa(uuid: str, novoSaldo: float, jwt_token: str = Heade
         logging.info("recording cash start:" + jwt_token)
 
 
-        caixa = crud.update_novo_saldoInicial(uuid = uuid, novoSaldo=novoSaldo)
+        caixa = crud.update_novo_saldoInicial(uuid = uuid, novoSaldo=data.novoSaldo)
 
         if caixa is None:
             return JSONResponse(
@@ -115,5 +158,3 @@ def update_saldoIncial_caixa(uuid: str, novoSaldo: float, jwt_token: str = Heade
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"message": "Erro ao atualizar o saldo: " + str(e)}
         )
-
-    
