e7e986116315001c41ebb48bf9144b3c0bfe8f97
Lucas Lopes <lucaslopes072013@gmail.com>
2023-12-13 17:05:38 -0300

    Add store to cliente data

--- frontend/electron-vite-app/src/renderer/src/store.js
+++ frontend/electron-vite-app/src/renderer/src/store.js
@@ -35,3 +35,70 @@ export const useAuthStore = defineStore('userData', {
     }
   });
 
+export const useClienteStore = defineStore('clienteData', {
+  id: 'cliente',
+
+  state: () => ({
+      idCliente: null,
+      nome: null,
+      email: null,
+      telefone: null,
+      cpf: null,
+      data_nascimento: null,
+      endereco: null,
+      saldo: null,
+      pontos: null
+  }),
+  
+  getters: {
+      getNome() {
+        return this.nome;
+          },
+      getEmail() {
+        return this.email;
+      },
+      getTelefone() {
+        return this.telefone;
+      },
+      getCpf() {
+        return this.cpf;
+          },
+      getDataNascimento() {
+        return this.data_nascimento;
+      },
+      getEndereco() {
+        return this.endereco;
+      },
+      getSaldo() {
+        return this.saldo;
+          },
+      getPontos() {
+        return this.pontos;
+      }
+  },
+  actions: {
+    saveClienteInfo({idCliente, nome, email, telefone, cpf, data_nascimento, endereco, saldo, pontos}){
+      this.idCliente = idCliente;
+      this.nome = nome;
+      this.email = email;
+      this.telefone = telefone;
+      this.cpf = cpf;
+      this.data_nascimento = data_nascimento;
+      this.endereco = endereco;
+      this.saldo = saldo;
+      this.pontos = pontos;
+    },
+
+    reset() {
+      this.idCliente = null;
+      this.nome = null;
+      this. email = null;
+      this.telefone = null;
+      this.cpf = null;
+      this.data_nascimento = null;
+      this.endereco = null;
+      this.saldo = null;
+      this.pontos = null;
+    }
+  }
+});
