b820b075d3849f227ca81120dac1d8ca50a891cd
Lucas Lopes <lucaslopes072013@gmail.com>
2023-12-15 16:04:27 -0300

    Create route to all funcionario pages

--- frontend/electron-vite-app/src/renderer/src/router.js
+++ frontend/electron-vite-app/src/renderer/src/router.js
@@ -5,6 +5,9 @@ import Dashboard from './components/Dashboard.vue'
 import Clientes from './components/Clientes.vue'
 import ClientesInfo from './components/ClientesInfo.vue'
 import CadastrarCliente from './components/CadastrarCliente.vue'
+import Funcionarios from './components/Funcionarios.vue'
+import FuncionariosInfo from './components/FuncionariosInfo.vue'
+import CadastrarFuncionario from './components/CadastrarFuncionario.vue'
 
 
 
@@ -38,7 +41,22 @@ const routes = [
         path: 'cadastrar-cliente', 
         name: 'cadastrarCliente', 
         component: CadastrarCliente,
-      }
+      },
+      { 
+        path: 'funcionarios', 
+        name: 'funcionarios', 
+        component: Funcionarios,
+      },
+      { 
+        path: 'ver-funcionario', 
+        name: 'funcionariosInfo', 
+        component: FuncionariosInfo,
+      },
+      { 
+        path: 'cadastrar-funcionario', 
+        name: 'cadastrarFuncionario', 
+        component: CadastrarFuncionario,
+      },
     ]
   },
 ];
