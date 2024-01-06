2c0b89fa528a64eb40c6f8348d2d3bfed91a9772
EuricoDNJR <massaultyplay@gmail.com>
2024-01-06 17:18:23 -0300

    reformulate to adapt discount logic in balance client cancel

--- app/database/crud.py
+++ app/database/crud.py
@@ -437,8 +437,13 @@ def update_balance_client_cancel(pedido):
             # Verifica se nos produtos do pedido há algum com a categoria Açaí para contabilizar no saldo do cliente
             for produto_pedido in produtos_pedidos:
                 if produto_pedido.idProduto.categoria == 'Açaí':
-                    pedido.idCliente.saldo -= produto_pedido.idProduto.valorVenda * produto_pedido.quantidade
-                    print(pedido.idCliente.saldo)
+                    if produto_pedido.desconto > Decimal(0.0):
+                        pedido.idCliente.saldo += Decimal(150)
+                        pedido.idCliente.saldo -= produto_pedido.idProduto.valorVenda * produto_pedido.quantidade - produto_pedido.desconto
+                        if produto_pedido.desconto <= Decimal(15):
+                            pedido.idCliente.saldo -= Decimal(15) - produto_pedido.desconto
+                    else:    
+                        pedido.idCliente.saldo -= produto_pedido.idProduto.valorVenda * produto_pedido.quantidade
                     pedido.idCliente.save()
             return True
         else:
