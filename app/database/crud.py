01efe2def5553ec9a95aaaa296dd9f0b69113455
EuricoDNJR <massaultyplay@gmail.com>
2024-01-23 11:25:50 -0300

    :recycle: refactor: Ending

--- app/database/crud.py
+++ app/database/crud.py
@@ -558,37 +558,33 @@ def get_all_produtos_pedidos_by_id(idPedido):
         return None
 
 def update_balance_client_and_order(pedido):
-    try:
-        produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == pedido.idPedido)
+    produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == pedido.idPedido)
 
-        # Verifica se há produtos_pedidos
-        if produtos_pedidos.exists():
-            # Verifica se nos produtos do pedido há algum com a categoria Açaí para contabilizar no saldo do cliente
-            for produto_pedido in produtos_pedidos:
-                if produto_pedido.idProduto.categoria.nome == 'Açaí' and verifier_client_promotion(pedido):
-                    if produto_pedido.desconto > Decimal(0.0):
-                        pedido.idCliente.saldo -= Decimal(150)
-                        if produto_pedido.valorTotal < Decimal(15):
-                            pedido.idCliente.saldo += Decimal(15) - produto_pedido.desconto
-                        produto_pedido.valorTotal -= produto_pedido.desconto
-                        produto_pedido.save() 
-                        pedido.idCliente.saldo += produto_pedido.valorTotal
-                        pedido.idPagamento.valorTotal += produto_pedido.valorTotal
-                    else:    
-                        pedido.idCliente.saldo += produto_pedido.valorTotal
-                        pedido.idPagamento.valorTotal += produto_pedido.valorTotal
-                    pedido.idCliente.save()
-                    pedido.idPagamento.save()
-                else:
-                    #Se for visitante, não contabiliza o saldo do cliente
+    # Verifica se há produtos_pedidos
+    if produtos_pedidos.exists():
+        # Verifica se nos produtos do pedido há algum com a categoria Açaí para contabilizar no saldo do cliente
+        for produto_pedido in produtos_pedidos:
+            if produto_pedido.idProduto.categoria.nome == 'Açaí' and verifier_client_promotion(pedido):
+                if produto_pedido.desconto > Decimal(0.0):
+                    pedido.idCliente.saldo -= Decimal(150)
+                    if produto_pedido.valorTotal < Decimal(15):
+                        pedido.idCliente.saldo += Decimal(15) - produto_pedido.desconto
+                    produto_pedido.valorTotal -= produto_pedido.desconto
+                    produto_pedido.save() 
+                    pedido.idCliente.saldo += produto_pedido.valorTotal
                     pedido.idPagamento.valorTotal += produto_pedido.valorTotal
-                    pedido.idPagamento.save()
-            return True
-        else:
-            # Se não houver produtos_pedidos, retorna None
-            return None
-    except DoesNotExist:
-        # Se ocorrer uma exceção DoesNotExist, retorna None
+                else:    
+                    pedido.idCliente.saldo += produto_pedido.valorTotal
+                    pedido.idPagamento.valorTotal += produto_pedido.valorTotal
+                pedido.idCliente.save()
+                pedido.idPagamento.save()
+            else:
+                #Se for visitante, não contabiliza o saldo do cliente
+                pedido.idPagamento.valorTotal += produto_pedido.valorTotal
+                pedido.idPagamento.save()
+        return True
+    else:
+        # Se não houver produtos_pedidos, retorna None
         return None
 
 def update_balance_client_and_order_unique(pedido, produto_instance):
@@ -617,38 +613,34 @@ def update_balance_client_and_order_unique(pedido, produto_instance):
         return None
 
 def update_balance_client_and_order_cancel(pedido):
-    try:
-        produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == pedido.idPedido)
+    produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == pedido.idPedido)
 
-        # Verifica se há produtos_pedidos
-        if produtos_pedidos.exists():
-            # Verifica se nos produtos do pedido há algum com a categoria Açaí para contabilizar no saldo do cliente
-            for produto_pedido in produtos_pedidos:
-                if produto_pedido.idProduto.categoria.nome == 'Açaí' and verifier_client_promotion(pedido):
-                    if produto_pedido.desconto > Decimal(0.0):
-                        pedido.idCliente.saldo += Decimal(150)
-                        produto_pedido.valorTotal += produto_pedido.desconto
-                        produto_pedido.save()
-                        pedido.idCliente.saldo -= Decimal(15) - produto_pedido.desconto
-                        if produto_pedido.valorTotal > Decimal(15):
-                            pedido.idCliente.saldo -= produto_pedido.valorTotal - Decimal(15)
-                        pedido.idPagamento.valorTotal -= produto_pedido.valorTotal
-                        pedido.idPagamento.save()
-                    else:    
-                        pedido.idCliente.saldo -= produto_pedido.valorTotal
-                        pedido.idPagamento.valorTotal -= produto_pedido.valorTotal
-                        pedido.idPagamento.save()
-                    pedido.idCliente.save()
-                else:
-                    #Se for visitante ou ate mesmo produto normal, so decrementa do valor total do pedido
+    # Verifica se há produtos_pedidos
+    if produtos_pedidos.exists():
+        # Verifica se nos produtos do pedido há algum com a categoria Açaí para contabilizar no saldo do cliente
+        for produto_pedido in produtos_pedidos:
+            if produto_pedido.idProduto.categoria.nome == 'Açaí' and verifier_client_promotion(pedido):
+                if produto_pedido.desconto > Decimal(0.0):
+                    pedido.idCliente.saldo += Decimal(150)
+                    produto_pedido.valorTotal += produto_pedido.desconto
+                    produto_pedido.save()
+                    pedido.idCliente.saldo -= Decimal(15) - produto_pedido.desconto
+                    if produto_pedido.valorTotal > Decimal(15):
+                        pedido.idCliente.saldo -= produto_pedido.valorTotal - Decimal(15)
                     pedido.idPagamento.valorTotal -= produto_pedido.valorTotal
                     pedido.idPagamento.save()
-            return True
-        else:
-            # Se não houver produtos_pedidos, retorna None
-            return None
-    except DoesNotExist:
-        # Se ocorrer uma exceção DoesNotExist, retorna None
+                else:    
+                    pedido.idCliente.saldo -= produto_pedido.valorTotal
+                    pedido.idPagamento.valorTotal -= produto_pedido.valorTotal
+                    pedido.idPagamento.save()
+                pedido.idCliente.save()
+            else:
+                #Se for visitante ou ate mesmo produto normal, so decrementa do valor total do pedido
+                pedido.idPagamento.valorTotal -= produto_pedido.valorTotal
+                pedido.idPagamento.save()
+        return True
+    else:
+        # Se não houver produtos_pedidos, retorna None
         return None
         
 def update_cliente(uuid, telefone=None, email=None, nome=None, dataNascimento=None, cpf=None, endereco=None, saldo=None):
@@ -831,16 +823,13 @@ def update_quantity_product(uuid, quantidade):
         return None
 
 def sum_all_stock_by_product(uuid_product):
-    try:
-        total = Decimal(0.0)
-        estoques = models.Estoque.select().where(models.Estoque.idProduto == uuid_product)
-        if estoques.exists():
-            for estoque in estoques:
-                total += estoque.quantidade
-            return total
-        else:
-            return None
-    except DoesNotExist:
+    total = Decimal(0.0)
+    estoques = models.Estoque.select().where(models.Estoque.idProduto == uuid_product)
+    if estoques.exists():
+        for estoque in estoques:
+            total += estoque.quantidade
+        return total
+    else:
         return None
         
 def update_stock(idEstoque, idProduto, quantidade=None, dataEntrada=None, dataVencimento=None, observacoes=None):
@@ -994,29 +983,23 @@ def delete_stock_registre(uuid):
         return None
 
 def delete_replace_quantity_product(idPedido):
-    try:
-        produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == idPedido)
-        if produtos_pedidos.exists():
-            for produto_pedido in produtos_pedidos:
-                produto = get_product_by_id(produto_pedido.idProduto.idProduto)
-                produto.quantidade += produto_pedido.quantidade
-                produto.save()
-            return True
-        else:
-            return None
-    except DoesNotExist:
+    produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == idPedido)
+    if produtos_pedidos.exists():
+        for produto_pedido in produtos_pedidos:
+            produto = get_product_by_id(produto_pedido.idProduto.idProduto)
+            produto.quantidade += produto_pedido.quantidade
+            produto.save()
+        return True
+    else:
         return None
 
 def delete_produto_pedido(idPedido):
-    try:
-        produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == idPedido)
-        if produtos_pedidos.exists():
-            for produto_pedido in produtos_pedidos:
-                produto_pedido.delete_instance()
-            return True
-        else:
-            return None
-    except DoesNotExist:
+    produtos_pedidos = models.ProdutoPedido.select().where(models.ProdutoPedido.idPedido == idPedido)
+    if produtos_pedidos.exists():
+        for produto_pedido in produtos_pedidos:
+            produto_pedido.delete_instance()
+        return True
+    else:
         return None
 
 def delete_pagamento(idPagamento):
