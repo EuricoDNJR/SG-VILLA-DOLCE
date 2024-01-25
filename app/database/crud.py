f5335fa79ef7be35930559f11e0315cb6765462e
EuricoDNJR <massaultyplay@gmail.com>
2024-01-24 21:56:35 -0300

    :recycle: refactor: Product CRUD funcs

--- app/database/crud.py
+++ app/database/crud.py
@@ -3,9 +3,6 @@ from peewee import DoesNotExist
 from passlib.hash import bcrypt
 from decimal import Decimal
 
-def create_produto(nome, descricao, categoria, valorVenda):
-    return models.Produto.create(nome=nome, descricao=descricao, categoria=categoria, valorVenda=valorVenda)
-
 def create_estoque(idProduto, quantidade, dataEntrada, dataVencimento, observacoes):
     return models.Estoque.create(idProduto=idProduto, quantidade=quantidade, dataEntrada=dataEntrada, dataVencimento=dataVencimento, observacoes=observacoes)
 
@@ -165,13 +162,6 @@ def get_all_cargos():
         # Se não houver cargos, retorna None
         return None
 
-def get_product_by_id(uuid):
-    try:
-        produto = models.Produto.get(models.Produto.idProduto == uuid)
-        return produto
-    except DoesNotExist:
-        return None
-
 def get_all_estoques_by_product(uuid):
     
     estoques = models.Estoque.select().where(models.Estoque.idProduto == uuid)
@@ -241,29 +231,6 @@ def get_tipo_pagamento_by_id(uuid):
 
         return tipo_pagamento
     except DoesNotExist:
-        return None
-
-def get_all_produtos():
-    # Tenta buscar todos os produtos
-    produtos = models.Produto.select()
-
-    # Verifica se há produtos
-    if produtos.exists():
-        # Retorna a lista de produtos se houver algum
-        return [
-            {
-                "idProduto": str(produto.idProduto),
-                "nome": produto.nome,
-                "descricao": produto.descricao if produto.descricao is not None else None,
-                "categoria": produto.categoria.nome,
-                "valorVenda": str(produto.valorVenda),
-                "unidadeMedida": produto.categoria.unidadeMedida,
-                "quantidade": str(produto.quantidade)
-            }
-            for produto in produtos
-        ]
-    else:
-        # Se não houver produtos, retorna None
         return None
 
 def get_all_estoques():
@@ -517,40 +484,6 @@ def update_novo_saldoInicial(uuid, novoSaldo = None):
     except DoesNotExist:
         return None
 
-def update_product(uuid, nome=None, descricao=None, categoria=None, valorCusto=None, valorVenda=None, unidadeMedida=None):
-    try:
-        produto = models.Produto.get(models.Produto.idProduto == uuid)
-        if produto is None:
-            return None
-        # Atualiza os atributos fornecidos
-        if nome is not None:
-            produto.nome = nome
-        if descricao is not None:
-            produto.descricao = descricao
-        if categoria is not None:
-            produto.categoria.nome = categoria
-            produto.categoria.save()
-        if valorVenda is not None:
-            produto.valorVenda = valorVenda
-        if unidadeMedida is not None:
-            produto.categoria.unidadeMedida = unidadeMedida
-            produto.categoria.save()
-
-        produto.save()
-
-        return {
-            "idProduto": str(produto.idProduto),
-            "nome": produto.nome,
-            "descricao": produto.descricao if produto.descricao is not None else None,
-            "categoria": produto.categoria.nome,
-            "valorVenda": str(produto.valorVenda),
-            "unidadeMedida": produto.categoria.unidadeMedida,
-            "quantidade": str(produto.quantidade)
-        }
-
-    except DoesNotExist:
-        return None
-
 def update_cargo(uuid, nome=None):
     try:
         cargo = models.Cargo.get(models.Cargo.idCargo == uuid)
@@ -690,17 +623,6 @@ def update_tipo_pagamento(uuid, nome=None):
             "nome": tipo_pagamento.nome
         }
 
-    except DoesNotExist:
-        return None
-
-def delete_product(uuid):
-    try:
-
-        models.Estoque.delete().where(models.Estoque.idProduto == uuid).execute()
-
-        produto = models.Produto.get(models.Produto.idProduto == uuid)
-        produto.delete_instance()
-        return True
     except DoesNotExist:
         return None
 
