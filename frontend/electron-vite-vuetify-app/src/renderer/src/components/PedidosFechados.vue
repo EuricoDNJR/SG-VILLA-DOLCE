406a6a592c67d1ac29d89a74035ce9abbf5444e0
Lucas Lopes <lucaslopes072013@gmail.com>
2024-01-31 00:25:23 -0300

    Create PedidosFechados.vue

---
+++ frontend/electron-vite-vuetify-app/src/renderer/src/components/PedidosFechados.vue
@@ -0,0 +1,102 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { fetchGet, getAuthToken, setMessageSnackbar } from '../utils/common';
+  
+  const pedidosFechados = ref([]);
+  const searchText = ref('');
+  const headers = [
+    { title: 'Colaborador', key: 'nomeUsuario' },
+    { title: 'Cliente', key: 'nomeCliente' },
+    { title: 'Telefone do Cliente', key: 'telefoneCliente' },
+    { title: 'Valor Total', key: 'valorTotal' },
+    { title: 'Status', key: 'status' },
+    { title: 'Tipo de Pagamento', key: 'tipoPagamento' },
+    { title: 'Valor Recebido', key: 'valorRecebimento'},
+    { title: 'Valor Devolvido (troco)', key: 'valorDevolvido'},
+  ];
+  
+  async function requestPedidosFechados(){
+    try{
+      const url = 'http://127.0.0.1:8000/v1/pedido/get_all_paid_and_canceled_orders/';
+      const token = getAuthToken();
+      
+      const response = await fetchGet(url, token);
+
+      if(response.status != 204){
+        const responseJson = await response.json();
+
+        if(response.status == 200){
+          pedidosFechados.value = responseJson;
+        }else{
+          setMessageSnackbar(responseJson.message, 'warning');
+        }
+      }
+    }catch(e){
+      console.log(e);
+      setMessageSnackbar("Falha ao carregar pedidos fechados", 'warning');
+    }
+  }
+
+  function getColorStatus(status){
+    let color = 'white';
+    
+    if(status == 'Pago'){
+      color = 'green';
+    }else if(status == 'Pendente'){
+      color = 'yellow';
+    }else if(status == 'Cancelado'){
+      color = 'red';
+    }
+
+    return color;
+  }
+
+  onMounted(() => requestPedidosFechados());
+</script>
+
+<template>
+    <v-toolbar color="grey-lighten-4" class="pa-4">
+      <v-text-field
+        v-model="searchText"
+        label="Pesquisar"
+        prepend-inner-icon="mdi-magnify"
+        variant="solo"
+        hide-details
+      ></v-text-field>
+    </v-toolbar>
+
+   <div
+      class="pa-4" 
+      color="grey-lighten-4"
+    >
+      <v-data-table-virtual
+        :headers="headers"
+        :items="pedidosFechados"
+        :search="searchText"
+        hide-no-data
+        hover
+        class="elevation-2 rounded"
+      >
+        <template v-slot:item.status="{ value }">
+          <v-chip :color="getColorStatus(value)">
+            {{ value }}
+          </v-chip>
+        </template>
+
+        <template v-slot:item.valorTotal="{ value }">
+          {{ value.replace(".", ",") }}
+        </template>
+
+        <template v-slot:item.valorRecebimento="{ value }">
+          {{ value.replace(".", ",") }}
+        </template>
+
+        <template v-slot:item.valorDevolvido="{ value }">
+          {{ value.replace(".", ",") }}
+        </template>
+      </v-data-table-virtual>
+    </div>
+</template>
+
+<style scoped>
+</style>
