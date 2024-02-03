c7f4a181cf5fa497f09163a2a46956327e60da86
Lucas Lopes <lucaslopes072013@gmail.com>
2024-02-03 16:44:02 -0300

    Remove .limit
    Para retornar todas as categorias

--- app/database/crud/dashboard.py
+++ app/database/crud/dashboard.py
@@ -7,15 +7,14 @@ def categorias_mais_vendidas():
     query = (
         models.Categoria.select(
             models.Categoria.nome,
-            fn.SUM(models.ProdutoPedido.quantidade).alias("total_vendido"),
+            fn.SUM(models.ProdutoPedido.valorTotal).alias("total_vendido"),
         )
         .join(models.Produto)
         .join(models.ProdutoPedido)
         .join(models.Pedido)
         .where(models.Pedido.data_criacao > datetime.now() - timedelta(days=180))
         .group_by(models.Categoria.nome)
-        .order_by(fn.SUM(models.ProdutoPedido.quantidade).desc())
-        .limit(5)
+        .order_by(fn.SUM(models.ProdutoPedido.valorTotal).desc())
     )
     return [
         {"categoria": categoria.nome, "total_vendido": str(categoria.total_vendido)}
